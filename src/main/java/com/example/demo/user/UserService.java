cc3f17989100b01a8741b0b4f7c6e1cbe1e131b1
clew <pkjh4567@gmail.com>
2019-06-25 13:27:05 +0900

    1234

---
+++ src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,16 @@
+package com.example.demo.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	@Autowired
+	UserMapper userMapper;
+	
+	public List<UserVO> getUserName() {
+		return userMapper.listMap();
+	}
+}
